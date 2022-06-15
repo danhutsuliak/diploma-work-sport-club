9c1bf913ad51be9c00bff40f9a416faf66fcd469
Daniel Hutsuliak <danguts23@gmail.com>
2022-06-15 22:17:40 +0300

    setup backend on express and hosting frontend in client folder and res api with all the data needed, also added sorting to stats table on stats page

---
+++ client/src/components/news-preview/news-preview.component.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+
+import "./news-preview.styles.scss";
+import { Link } from "react-router-dom";
+import NewsBlock from "../news-block/news-block.component";
+
+const NewsPreview = () => {
+  const [news, setNews] = useState(null);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => {
+        setNews(data.news);
+      });
+  }, []);
+
+  return (
+    <div className="news-preview" id="news-preview">
+      <h2>
+        <Link to="/news">Новини</Link>
+      </h2>
+
+      <div className="news-wrapper">
+        {news?.map((post) => {
+          return (
+            <Link key={post.id} to={`/news/${post.id}`}>
+              <NewsBlock
+                image={post.featuredImage}
+                title={post.title}
+                text={post.description}
+                date={post.date}
+              />
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default NewsPreview;
